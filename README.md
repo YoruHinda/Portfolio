0e35fdb214251b4bc385e2f33a5bb868a88010c8
Ruan Phablo <ruanphablo778@gmail.com>
2023-04-19 20:35:23 -0300

    Feat: add js

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 ## Description
 
+I created my first web portfolio to demonstrate my main projects.
+
 ## How To access
 
+Link: 
+
 ## License
+
+This project is under the MIT License. See the file LICENSE for more details.
---
+++ assets/js/index.js
@@ -0,0 +1,98 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    let aboutbutton = document.getElementById("aboutbutton")
+    let skillbutton = document.getElementById('skillbutton')
+    let projectbutton = document.getElementById('projectbutton')
+    let contactbutton = document.getElementById('contactbutton')
+    aboutbutton.addEventListener('click', displayabout)
+    skillbutton.addEventListener('click', displayskill)
+    projectbutton.addEventListener('click', displayproject)
+    contactbutton.addEventListener('click', displaycontact)
+});
+
+function displayabout() {
+    let aboutsection = document.getElementById('about')
+    let skillsection = document.getElementById('skill')
+    let projectsection = document.getElementById('project')
+    let contactsection = document.getElementById('contact')
+    if (skillsection.style.width != '0' || projectsection.style.width != '0' || contactsection.style.width != '0') {
+        skillsection.style.width = '0'
+        projectsection.style.width = '0'
+        contactsection.style.width = '0'
+        skillsection.style.padding = '0'
+        projectsection.style.padding = '0'
+        contactsection.style.padding = '0'
+    }
+    if (aboutsection.style.width == '40vw') {
+        aboutsection.style.width = '0'
+        aboutsection.style.padding = '0'
+    } else {
+        aboutsection.style.width = '40vw'
+        aboutsection.style.padding = '30px'
+    }
+}
+
+function displayskill() {
+    let aboutsection = document.getElementById('about')
+    let skillsection = document.getElementById('skill')
+    let projectsection = document.getElementById('project')
+    let contactsection = document.getElementById('contact')
+    if (aboutsection.style.width != '0' || projectsection.style.width != '0' || contactsection.style.width != '0') {
+        aboutsection.style.width = '0'
+        projectsection.style.width = '0'
+        contactsection.style.width = '0'
+        aboutsection.style.padding = '0'
+        projectsection.style.padding = '0'
+        contactsection.style.padding = '0'
+    }
+    if (skillsection.style.width == '40vw') {
+        skillsection.style.width = '0'
+        skillsection.style.padding = '0'
+    } else {
+        skillsection.style.width = '40vw'
+        skillsection.style.padding = '30px'
+    }
+}
+
+function displayproject() {
+    let aboutsection = document.getElementById('about')
+    let skillsection = document.getElementById('skill')
+    let projectsection = document.getElementById('project')
+    let contactsection = document.getElementById('contact')
+    if (aboutsection.style.width != '0' || skillsection.style.width != '0' || contactsection.style.width != '0') {
+        aboutsection.style.width = '0'
+        skillsection.style.width = '0'
+        contactsection.style.width = '0'
+        aboutsection.style.padding = '0'
+        skillsection.style.padding = '0'
+        contactsection.style.padding = '0'
+    }
+    if (projectsection.style.width == '40vw') {
+        projectsection.style.width = '0'
+        projectsection.style.padding = '0'
+    } else {
+        projectsection.style.width = '40vw'
+        projectsection.style.padding = '30px'
+    }
+}
+
+function displaycontact() {
+    let aboutsection = document.getElementById('about')
+    let skillsection = document.getElementById('skill')
+    let projectsection = document.getElementById('project')
+    let contactsection = document.getElementById('contact')
+    if (aboutsection.style.width != '0' || projectsection.style.width != '0' || skillsection.style.width != '0') {
+        aboutsection.style.width = '0'
+        projectsection.style.width = '0'
+        skillsection.style.width = '0'
+        aboutsection.style.padding = '0'
+        projectsection.style.padding = '0'
+        skillsection.style.padding = '0'
+    }
+    if (contactsection.style.width == '40vw') {
+        contactsection.style.width = '0'
+        contactsection.style.padding = '0'
+    } else {
+        contactsection.style.width = '40vw'
+        contactsection.style.padding = '30px'
+    }
+}
